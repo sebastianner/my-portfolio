22a344e14f8862b8204ee833ad1eca77f0e6b6d3
sebastian <sebastianner23@gmail.com>
2024-03-16 23:39:11 -0500

    hamburger menu option list animation

--- src/components/HamburgerMenu/HamburgerMenu.tsx
+++ src/components/HamburgerMenu/HamburgerMenu.tsx
@@ -1,7 +1,8 @@
 import classNames from "classnames";
 import { NavBarConstants } from "../NavBar/constants";
 import HamburgerMenuIcon from "../HamburgerMenuIcon/HamburgerMenuIcon";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef } from "react";
+import styles from "./HamburgerMenu.module.css";
 
 type Props = {
   className?: string;
@@ -10,15 +11,35 @@ type Props = {
   setShowMobile: Dispatch<SetStateAction<boolean>>;
 };
 
+const fadeIn = (menuOption: ChildNode, delay: number) => {
+  if (menuOption instanceof Element) {
+    setTimeout(() => {
+      menuOption.classList.add(styles.fadein);
+    }, delay);
+  }
+};
+
 function HamburgerMenu({
   className,
   color = "#fff",
   showMobile,
   setShowMobile,
 }: Props) {
+  const menuListRef = useRef<HTMLUListElement>(null);
+
   const handleSetShowMobile = () => {
     setShowMobile(!showMobile);
   };
+
+  useEffect(() => {
+    const menuOptions = menuListRef.current?.childNodes;
+    if (showMobile) {
+      menuOptions?.forEach((menuOption, index) => {
+        fadeIn(menuOption, index * 200);
+      });
+    }
+  }, [showMobile]);
+
   return (
     <aside
       className={classNames(
@@ -28,6 +49,7 @@ function HamburgerMenu({
         "font-semibold text-2xl",
         "bg-[#051C11DB]",
         { "bg-transparent h-0": !showMobile },
+        styles.hamburgerMenu,
         className
       )}
       style={{ color: color }}
@@ -38,30 +60,39 @@ function HamburgerMenu({
         }}
         className="absolute top-[25px] right-[40px]"
       >
-        <HamburgerMenuIcon
-          className={classNames({ "hidden ": !showMobile })}
-          isOpen={showMobile}
-        />
+        <HamburgerMenuIcon className={"md:hidden"} isOpen={showMobile} />
       </button>
 
-      <ul
-        className={classNames("flex flex-col gap-5", {
-          "hidden ": !showMobile,
-        })}
-      >
-        <a href={"#home"} onClick={handleSetShowMobile}>
+      <ul className={classNames("flex flex-col gap-5")} ref={menuListRef}>
+        <a
+          href={"#home"}
+          onClick={handleSetShowMobile}
+          className={classNames(styles.fadeItem, { "opacity-0": !showMobile })}
+        >
           <li>{NavBarConstants.home}</li>
         </a>
-        <a href={"#about"} onClick={handleSetShowMobile}>
+        <a
+          href={"#about"}
+          onClick={handleSetShowMobile}
+          className={classNames(styles.fadeItem, { "opacity-0": !showMobile })}
+        >
           <li>{NavBarConstants.aboutMe}</li>
         </a>
-        <a href={"#work"} onClick={handleSetShowMobile}>
+        <a
+          href={"#work"}
+          onClick={handleSetShowMobile}
+          className={classNames(styles.fadeItem, { "opacity-0": !showMobile })}
+        >
           <li>{NavBarConstants.experience}</li>
         </a>
         {/* <a href={"#projects"} onClick={handleSetShowMobile}>
             <li>Projects</li>
           </a> */}
-        <a href={"#contact"} onClick={handleSetShowMobile}>
+        <a
+          href={"#contact"}
+          onClick={handleSetShowMobile}
+          className={classNames(styles.fadeItem, { "opacity-0": !showMobile })}
+        >
           <li>{NavBarConstants.contact}</li>
         </a>
       </ul>
