64af7e5c56e71db193a50786c6015ddc54cb8201
sebastian <sebastianner23@gmail.com>
2025-02-04 22:27:37 -0500

    update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.scss";
 import { Metadata } from "next";
 import { Inter, Montserrat } from "next/font/google";
-import { getDocuments } from "outstatic/server";
 
 const inter = Inter({ subsets: ["latin"] });
 const Poppins_Font = Montserrat({
@@ -14,9 +13,6 @@ export const metadata: Metadata = {
   description: "Sebastian Mera Frontend developer portfolio",
   title: "Sebastian M - Portfolio",
 };
-
-const posts = getDocuments("posts", ["title"]);
-console.log("here" + posts);
 
 export default function RootLayout({
   children,
