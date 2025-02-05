e1248bba1ef659999ff3d63146f9ae4fc68b36a0
sebastian <sebastianner23@gmail.com>
2025-02-04 22:37:45 -0500

    remove outlastic

--- src/app/test/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getCollections } from "outstatic/server"; // Replace with actual import path based on Outstatic’s docs
-
-export default function Page() {
-  const collections = getCollections();
-  console.log(collections);
-
-  return <div>{collections}</div>;
-}
